d0eae28eaa4c7cb0318ec7e52efd2743021574da
en-Barry <en_barry@icloud.com>
2021-08-27 03:50:00 +0900

    Add: コントローラのロジックとseedデータの追加
    APIのデータ受け渡しのため

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -1,2 +1,43 @@
-class ContestsController < ApplicationController
+class Api::V1::ContestsController < ApplicationController
+  before_action :set_contest, only: %i[update destroy]
+
+  def index
+    contests = Contest.all
+    render json: contests
+  end
+
+  def create
+    contest = Contest.new(contest_params)
+    if contest.save
+      render json: contest
+    else
+      render json: contest.errors
+    end
+  end
+
+  def update
+    if @contest.update(contest_params)
+      render json: @contest
+    else
+      render json: @contest.errors
+    end
+  end
+
+  def destroy
+    if @contest.destroy
+      render json: @contest
+    else
+      render json: @contest.errors
+    end
+  end
+
+  private
+
+  def set_contest
+    @contest = Contest.find(params[:id])
+  end
+
+  def contest_params
+    params.require(:contest).permit(:name, :year, :judge, :finalist, :score)
+  end
 end
---
+++ db/fixtures/development/contest.rb
@@ -0,0 +1,49 @@
+# M-1グランプリ
+
+# キングオブコント
+Contest.seed do |s|
+  s.id = 1
+  s.name = 'キングオブコント'
+  s.year = 2020
+  s.judge = '松本'
+  s.finalist = '滝音'
+  s.score = 86
+end
+
+Contest.seed do |s|
+  s.id = 2
+  s.name = 'キングオブコント'
+  s.year = 2020
+  s.judge = '大竹'
+  s.finalist = '滝音'
+  s.score = 89
+end
+
+Contest.seed do |s|
+  s.id = 3
+  s.name = 'キングオブコント'
+  s.year = 2020
+  s.judge = '三村'
+  s.finalist = '滝音'
+  s.score = 90
+end
+
+Contest.seed do |s|
+  s.id = 4
+  s.name = 'キングオブコント'
+  s.year = 2020
+  s.judge = '日村'
+  s.finalist = '滝音'
+  s.score = 90
+end
+
+Contest.seed do |s|
+  s.id = 5
+  s.name = 'キングオブコント'
+  s.year = 2020
+  s.judge = '設楽'
+  s.finalist = '滝音'
+  s.score = 90
+end
+
+# R-1グランプリ
