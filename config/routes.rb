a88728b224522fb618ec0f10a5e4e590866f4d45
en-Barry <en_barry@icloud.com>
2021-09-01 01:52:50 +0900

    Add: コンテストの年代とファイナリスト名を返すリクエストのルーティングを追加
    年代選択画面と得点入力画面で情報を取得するときに必要なため

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
     namespace 'v1' do
       resources :judgements
       get 'contests/m-1gp', to: 'contests#m_1gp'
-      get 'contests/king', to: 'contests#king_of_conte'
+      get 'contests/king_of_conte', to: 'contests#king_of_conte'
       get 'contests/r-1gp', to: 'contests#r_1gp'
+
+      get 'finalists/m-1gp', to: 'finalists#m_1gp'
+      get 'finalists/king_of_conte', to: 'finalists#king_of_conte'
+      get 'finalists/r-1gp', to: 'finalists#r_1gp'
     end
   end
 end
