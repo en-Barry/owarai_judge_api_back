6dd194a3b002dce485fef6a22bb257ce2b63fa0b
en-Barry <en_barry@icloud.com>
2021-09-04 00:46:56 +0900

    Fix: ルーティングのパスを修正
    (_)ではなく(-)がgoogle推奨のため

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
     namespace 'v1' do
       resources :judgements
       get 'contests/m-1gp', to: 'contests#m_1gp'
-      get 'contests/king_of_conte', to: 'contests#king_of_conte'
+      get 'contests/king-of-conte', to: 'contests#king_of_conte'
       get 'contests/r-1gp', to: 'contests#r_1gp'
 
-      get 'finalists/m-1gp', to: 'finalists#m_1gp'
-      get 'finalists/king_of_conte', to: 'finalists#king_of_conte'
-      get 'finalists/r-1gp', to: 'finalists#r_1gp'
+      get 'finalists/m-1gp(/:id)', to: 'finalists#m_1gp'
+      get 'finalists/king-of-conte(/:id)', to: 'finalists#king_of_conte'
+      get 'finalists/r-1gp(/:id)', to: 'finalists#r_1gp'
     end
   end
 end
