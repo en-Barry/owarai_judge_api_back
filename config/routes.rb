0a24dbe622ff55d90f515cfe203b84577c264771
en-Barry <en_barry@icloud.com>
2021-09-09 16:31:50 +0900

    Add: judgement内にルーティングを追加
    ファイナリスト情報取得のため
    judgementのidは必要ないためcollectionで省略

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  root 'application#root'
   namespace 'api' do
     namespace 'v1' do
-      resources :judgements
+      resources :judgements do
+        collection do
+          get 'm-1gp(/:id)', to: 'judgements#m_1gp'
+          get 'king-of-conte(/:id)', to: 'judgements#king_of_conte'
+          get 'r-1gp(/:id)', to: 'judgements#r_1gp'
+        end
+      end
+
       get 'contests/m-1gp', to: 'contests#m_1gp'
       get 'contests/king-of-conte', to: 'contests#king_of_conte'
       get 'contests/r-1gp', to: 'contests#r_1gp'
