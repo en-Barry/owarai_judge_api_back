a3c2ddbfdab1b082ae52d5250380a679ef61dc62
en-Barry <en_barry@icloud.com>
2021-09-26 02:52:28 +0900

    Fix: コントローラに書いていたロジックをscopeに記述
    ファットコントローラを避けるため

--- app/controllers/api/v1/finalists_controller.rb
+++ app/controllers/api/v1/finalists_controller.rb
@@ -2,20 +2,22 @@ class Api::V1::FinalistsController < ApplicationController
 
   def m_1gp
     contest = Contest.find_by(name: 0, year: params[:id])
-    finalists = Judgement.where(contest_id: contest).select('finalist_id').distinct.map { |f| f.finalist }
+    finalist_ids = Judgement.where(contest_id: contest).distinct.pluck(:finalist_id)
+    finalists = Finalist.where(id: finalist_ids).finalist_all
     render json: finalists
   end
 
   def king_of_conte
     contest = Contest.find_by(name: 1, year: params[:id])
     finalist_ids = Judgement.where(contest_id: contest).distinct.pluck(:finalist_id)
-    finalists = Finalist.where(id: finalist_ids).pluck(:id, :name).map { |id, name| { id: id, name: name }}
+    finalists = Finalist.where(id: finalist_ids).finalist_all
     render json: finalists
   end
 
   def r_1gp
     contest = Contest.find_by(name: 2, year: params[:id])
-    finalists = Judgement.where(contest_id: contest).select('finalist_id').distinct.map { |f| f.finalist }
+    finalist_ids = Judgement.where(contest_id: contest).distinct.pluck(:finalist_id)
+    finalists = Finalist.where(id: finalist_ids).finalist_all
     render json: finalists
   end
 end
--- app/models/finalist.rb
+++ app/models/finalist.rb
@@ -1,3 +1,5 @@
 class Finalist < ApplicationRecord
   has_many :judgements, foreign_key: "reference_id"
+
+  scope :finalist_all, -> { pluck(:id, :name).map { |id, name| { id: id, name: name }} }
 end
