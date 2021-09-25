bfd39af03460c6bd5c7a5fe8d4e909a07039194e
en-Barry <en_barry@icloud.com>
2021-09-25 23:03:54 +0900

    Add: 審査結果の処理を追加

--- app/controllers/api/v1/judgements_controller.rb
+++ app/controllers/api/v1/judgements_controller.rb
@@ -3,18 +3,24 @@ class Api::V1::JudgementsController < ApplicationController
   before_action :set_judgement, only: %i[update destroy]
 
   def create
-    judgement = Judgement.new(judge_id: current_judge.id) if current_judge.present?
-    judgement.attributes = judgement_params
+    judgements = judgements_params.to_h
     
-    if judgement.save
-      render json: judgement
+    if Judgement.find_by(judge_id: current_judge.id, contest_id: judgements[:judgements][0][:contest_id]).present?
+      render json: { errors: { title: '既に審査済みです。複数回の審査はアップデートで対応予定です。'}, status: :not_found }
     else
-      render json: judgement.errors
+      ActiveRecord::Base.transaction do
+        register_judge = judgements[:judgements].map do |judgement|
+          judgement[:judge_id].present? ? new_judge = Judgement.new(judge_id: judgement[:judge_id]) : new_judge = Judgement.new(judge_id: current_judge.id) 
+          new_judge.update_attributes(contest_id: judgement[:contest_id],
+                                      finalist_id: judgement[:finalist_id],
+                                      score: judgement[:score])
+        end
+      end
     end
   end
 
   def update
-    if @judgement.update(judgement_params)
+    if @judgement.update(judgements_params)
       render json: @judgement
     else
       render json: @judgement.errors
@@ -48,7 +54,7 @@ class Api::V1::JudgementsController < ApplicationController
     @judgement = Judgement.find(params[:id])
   end
 
-  def judgement_params
-    params.require(:judgement).permit(:contest_id, :judge_id, :finalist_id, :score)
+  def judgements_params
+    params.permit(judgements: [:contest_id, :judge_id, :finalist_id, :score])
   end
 end
