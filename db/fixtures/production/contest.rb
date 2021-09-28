7f36205f6343937e4db9cc23aaea18cc4a2e1625
en-Barry <en_barry@icloud.com>
2021-09-29 07:41:41 +0900

    Add: 本番用のデータを追加

---
+++ db/fixtures/production/contest.rb
@@ -0,0 +1,141 @@
+# M-1グランプリ name: 0
+Contest.seed_once(:id,
+  {
+    id: 1,
+    name: 0,
+    year: 2001
+  },
+  {
+    id: 2,
+    name: 0,
+    year: 2002
+  },
+  {
+    id: 3,
+    name: 0,
+    year: 2003
+  },
+  {
+    id: 4,
+    name: 0,
+    year: 2004
+  },
+  {
+    id: 5,
+    name: 0,
+    year: 2005
+  },
+  {
+    id: 6,
+    name: 0,
+    year: 2006
+  },
+  {
+    id: 7,
+    name: 0,
+    year: 2007
+  },
+  {
+    id: 8,
+    name: 0,
+    year: 2008
+  },
+  {
+    id: 9,
+    name: 0,
+    year: 2009
+  },
+  {
+    id: 10,
+    name: 0,
+    year: 2010
+  },
+  {
+    id: 11,
+    name: 0,
+    year: 2015
+  },
+  {
+    id: 12,
+    name: 0,
+    year: 2016
+  },
+  {
+    id: 13,
+    name: 0,
+    year: 2017
+  },
+  {
+    id: 14,
+    name: 0,
+    year: 2018
+  },
+  {
+    id: 15,
+    name: 0,
+    year: 2019
+  },
+  {
+    id: 16,
+    name: 0,
+    year: 2020  
+  },
+  {
+    id: 17,
+    name: 0,
+    year: 2021  
+  }
+)
+
+# キングオブコント name: 1
+Contest.seed_once(:id,
+  {
+    id: 30,
+    name: 1,
+    year: 2015
+  },
+  {
+    id: 31,
+    name: 1,
+    year: 2016
+  },
+  {
+    id: 32,
+    name: 1,
+    year: 2017
+  },
+  {
+    id: 33,
+    name: 1,
+    year: 2018
+  },
+  {
+    id: 34,
+    name: 1,
+    year: 2019
+  },
+  {
+    id: 35,
+    name: 1,
+    year: 2020
+  },
+  {
+    id: 36,
+    name: 1,
+    year: 2021
+  }
+)
+
+# R-1グランプリ name: 2
+Contest.seed_once(
+  {
+    id: 60,
+    name: 2,
+    year:2020
+  },
+  {
+    id: 61,
+    name: 2,
+    year:2021
+  }
+)
---
+++ db/fixtures/production/profile.rb
@@ -0,0 +1,32 @@
+Profile.seed_once(
+  {
+    id: 1,
+    judge_id: 1,
+    age: 5,
+    gender: 0,
+  },
+  {
+    id: 2,
+    judge_id: 2,
+    age: 5,
+    gender: 0,
+  },
+  {
+    id: 3,
+    judge_id: 3,
+    age: 5,
+    gender: 0,
+  },
+  {
+    id: 4,
+    judge_id: 4,
+    age: 4,
+    gender: 0,
+  },
+  {
+    id: 5,
+    judge_id: 5,
+    age: 4,
+    gender: 0,
+  }
+)
