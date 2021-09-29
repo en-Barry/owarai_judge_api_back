5008a61277231bd937a2ffd834af70b418d3f3c1
en-Barry <en_barry@icloud.com>
2021-09-30 01:21:44 +0900

    Add: seedデータの不足分を追加
    後から追加したカラムの情報を記述していなかったため

--- db/fixtures/production/judge.rb
+++ db/fixtures/production/judge.rb
@@ -2,27 +2,32 @@ Judge.seed_once(:id,
   {
     id: 1,
     name: '松本',
-    category: 1
+    category: 1,
+    judge_digest: ""
   },
   {
     id: 2,
     name: '大竹',
-    category: 1
+    category: 1,
+    judge_digest: ""
   },
   {
     id: 3,
     name: '三村',
-    category: 1
+    category: 1,
+    judge_digest: ""
   },
   {
     id: 4,
     name: '日村',
-    category: 1
+    category: 1,
+    judge_digest: ""
   },
   {
     id: 5,
     name: '設楽',
-    category: 1
+    category: 1,
+    judge_digest: ""
   },
   {
     id: 6,
