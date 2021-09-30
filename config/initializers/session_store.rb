cb4c8d3e206cc3434a696478cfbc10c50fd75e50
en-Barry <en_barry@icloud.com>
2021-09-30 21:01:57 +0900

    Fix: cookieを許可するドメインを明示、したのを戻す
    本番環境でcookieが正しく作動しないため、変わらなかったため

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_auth-app-api', domain: 'https://www.owarai-judge.com', expire_after: 3.years
+  Rails.application.config.session_store :cookie_store, key: '_auth-app-api', domain: :all, expire_after: 3.years
 else
   Rails.application.config.session_store :cookie_store, key: '_auth-app-api'
 end
