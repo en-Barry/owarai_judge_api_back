9531fffb6a92ca8526866b0c7d1918f7d80f6f25
en-Barry <en_barry@icloud.com>
2021-10-02 15:31:38 +0900

    Add: ファイナリスト情報取得時に順番を指定
    orderを指定しないと、finalistsテーブルに保存されてる順に取得してしまうため

--- app/models/finalist.rb
+++ app/models/finalist.rb
@@ -1,5 +1,5 @@
 class Finalist < ApplicationRecord
   has_many :judgements, foreign_key: "reference_id"
 
-  scope :finalist_all, -> { pluck(:id, :name).map { |id, name| { id: id, name: name }} }
+  scope :finalist_all, -> { order("field(id, #{finalist_ids.join(',')})").pluck(:id, :name).map { |id, name| { id: id, name: name }} }
 end
