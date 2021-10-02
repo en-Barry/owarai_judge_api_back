3a8a404bbce7306dc325992dc4d868e1d5bc3523
en-Barry <en_barry@icloud.com>
2021-10-02 15:39:38 +0900

    Fix: scopeに必要な引数を設定

--- app/controllers/api/v1/finalists_controller.rb
+++ app/controllers/api/v1/finalists_controller.rb
@@ -3,21 +3,21 @@ class Api::V1::FinalistsController < ApplicationController
   def m_1gp
     contest = Contest.find_by(name: 0, year: params[:id])
     finalist_ids = Judgement.where(contest_id: contest).distinct.pluck(:finalist_id)
-    finalists = Finalist.where(id: finalist_ids).finalist_all
+    finalists = Finalist.where(id: finalist_ids).finalist_all(finalist_ids)
     render json: finalists
   end
 
   def king_of_conte
     contest = Contest.find_by(name: 1, year: params[:id])
     finalist_ids = Judgement.where(contest_id: contest).distinct.pluck(:finalist_id)
-    finalists = Finalist.where(id: finalist_ids).finalist_all
+    finalists = Finalist.where(id: finalist_ids).finalist_all(finalist_ids)
     render json: finalists
   end
 
   def r_1gp
     contest = Contest.find_by(name: 2, year: params[:id])
     finalist_ids = Judgement.where(contest_id: contest).distinct.pluck(:finalist_id)
-    finalists = Finalist.where(id: finalist_ids).finalist_all
+    finalists = Finalist.where(id: finalist_ids).finalist_all(finalist_ids)
     render json: finalists
   end
 end
