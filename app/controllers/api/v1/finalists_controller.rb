5a48109db24f82b40509242c4fd4ceebe6466229
en-Barry <en_barry@icloud.com>
2021-10-08 22:47:36 +0900

    Fix: コードをDRYにするための修正

--- app/controllers/api/v1/finalists_controller.rb
+++ app/controllers/api/v1/finalists_controller.rb
@@ -1,21 +1,7 @@
 class Api::V1::FinalistsController < ApplicationController
 
-  def m_1gp
-    contest = Contest.find_by(name: 0, year: params[:id])
-    finalist_ids = Judgement.where(contest_id: contest).distinct.pluck(:finalist_id)
-    finalists = Finalist.where(id: finalist_ids).finalist_all(finalist_ids)
-    render json: finalists
-  end
-
-  def king_of_conte
-    contest = Contest.find_by(name: 1, year: params[:id])
-    finalist_ids = Judgement.where(contest_id: contest).distinct.pluck(:finalist_id)
-    finalists = Finalist.where(id: finalist_ids).finalist_all(finalist_ids)
-    render json: finalists
-  end
-
-  def r_1gp
-    contest = Contest.find_by(name: 2, year: params[:id])
+  def index
+    contest = Contest.find(params[:id])
     finalist_ids = Judgement.where(contest_id: contest).distinct.pluck(:finalist_id)
     finalists = Finalist.where(id: finalist_ids).finalist_all(finalist_ids)
     render json: finalists
--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,7 @@ Rails.application.routes.draw do
       get 'contests/king-of-conte', to: 'contests#king_of_conte'
       get 'contests/r-1gp', to: 'contests#r_1gp'
 
-      get 'finalists/m-1gp(/:id)', to: 'finalists#m_1gp'
-      get 'finalists/king-of-conte(/:id)', to: 'finalists#king_of_conte'
-      get 'finalists/r-1gp(/:id)', to: 'finalists#r_1gp'
+      get 'finalists(/:id)', to: 'finalists#index'
     end
   end
 end
